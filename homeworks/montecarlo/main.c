70c759a131f1bedade1f8c416cc8a6ad5211ad83
frejanielsen <fbn29@Frejas-pc.localdomain>
2021-06-26 13:50:07 +0200

    hi

--- homeworks/montecarlo/main.c
+++ homeworks/montecarlo/main.c
@@ -20,7 +20,7 @@ double b1[] = {5.0};
 int dim = sizeof(a1)/sizeof(a1[0]);
 int N = 10000;
 double res = 0;
-double err = 0;
+double err = 0.5;
 printf("A:\n");
 printf("checking function f1 works: %f\n",f1(dim,vars));
 
@@ -38,7 +38,7 @@ printf("plainmc called \nres=%f\nerr=%f\nactual result: 5.9933\n\n",res,err);
 	double f2(int dim, double* vars){
 		double x = vars[0];
 		double y = vars[1];
-		return cos(x)*cos(y)*sin(x);
+		return cos(x)*cos(y);
 		}
 
 double a2[] = {0.0, 0,0};
@@ -50,7 +50,7 @@ N = 30000;
 
 
 plainmc(dim,f2,a2,b2,N,&res,&err);
-printf("integral of cos(x)*cos(y)*sin(x) from 0 to pi , with 30000 points:\n");
+printf("integral of cos(x)*cos(y) from 0 to pi , with 30000 points:\n");
 printf("plainmc called. \nres=%f\nerr=%f\nactual result: 0\n\n",res,err);
 
 
@@ -84,16 +84,16 @@ for (int i=1;i<11;i++){
 
 dim=sizeof(a2)/sizeof(a2[0]);
 
-printf("multidimensional integration on integral of cos(x)*cos(y)*sin(x) from 0 to 3.14159265 in both x and y, with 30k points:");
+printf("multidimensional integration on integral of cos(x)*cos(y) from 0 to 3.14159265 in both x and y, with 30k points:");
 N=30000;
-multimc(dim,f2,a2,b2,N,&res,&err);
+multimc(dim,f3,a3,b3,N,&res,&err);
 printf("multimc called. \nres=%f\nerr=%f\nactual result: 0\n",res,err);
 
 FILE* err_file = fopen("out_err.txt","w");
 for (N=10;N<1e5;N+=1000){
-	multimc(dim,f2,a2,b2,N,&res,&err);
+	multimc(dim,f3,a3,b3,N,&res,&err);
 	fprintf(err_file,"%10i  %10f",N,err);
-	multimc(dim,f2,a2,b2,N,&res,&err);
+	multimc(dim,f3,a3,b3,N,&res,&err);
 	fprintf(err_file,"%10f\n",err);
 }
 
