1226b76d681d4a43a1de24b5d7c399db14533596
frejanielsen <fbn29@Frejas-pc.localdomain>
2021-06-27 22:50:35 +0200

    latex added

--- exercises/latex/main.c
+++ exercises/latex/main.c
@@ -1,28 +1,16 @@
 #include<stdio.h>
 #include<math.h>
 
-
-double ex(double x){
-	if(x<0){
-		return 1/ex(-x);
-		}
-
-	if(x>1./8){
-		return pow(ex(x/2),2);
-		}
-
-	return 1 + x * (1 + x / 2 * (1 + x / 3 * (1 + x / 4 * (1 + x / 5 * (1 + x / 6 * (1 + x / 7 * (1 + x / 8 * (1 + x / 9 * (1 + x / 10)))))))));
+double exponential(double x){
+	if(x<0)return 1/exponential(-x);
+	if(x>1./8)return pow(exponential(x/2),2);
+	return 1+x*(1+x/2*(1+x/3*(1+x/4*(1+x/5*(1+x/6*(1+x/7*(1+x/8*(1+x/9*(1+x/10)))))))));
 }
 
-
 int main(){
-
-	int x = -20;
-
-	while(x<=20){
-		printf("%10g %10g %10g\n", x/10.0, ex(x/10.0), exp(x/10.0));
-		x++;
-	}
+	for( double x = 1./16; x <= 8; x += 1./16 ){
+			printf("%g %g %g\n",x,exponential(x),exp(x));
+		}
 
 return 0;
 }
